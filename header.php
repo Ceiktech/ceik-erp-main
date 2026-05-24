2ecd06cc913e49492763fe6cad3c8ef8afde116e
Ceiktech <ceikpiaget@gmail.com>
2026-05-24 20:22:04 -0300

    test

--- header.php
+++ header.php
@@ -142,7 +142,7 @@ async function enviarMensagem() {
         formData.append('pergunta', texto);
 
         // Caminho gerado de forma absoluta e segura via PHP
-        const urlEndpoint = '<?php echo "http://" . $_SERVER['HTTP_HOST'] . "/ceik-erp/views/processachat.php"; ?>';
+        const urlEndpoint = '<?php echo "http://" . $_SERVER['HTTP_HOST'] . "/ceik-erp/processachat.php"; ?>';
         
         const response = await fetch(urlEndpoint, {
             method: 'POST',
