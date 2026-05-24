51a2d4d1460658dbe991358a4c542659bd9c5ed2
Ceiktech <ceikpiaget@gmail.com>
2026-05-24 00:41:13 -0300

    Corrigido caminho do scripts_chat.js no header.php

--- header.php
+++ header.php
@@ -10,7 +10,8 @@
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-        <script src="scripts_chat.js"></script>
+        <script src="js/scripts_chat.js"></script>
+
     </head>
     <body class="sb-nav-fixed">
         <?php
